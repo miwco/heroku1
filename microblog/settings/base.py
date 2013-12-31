60c31fbbf2f49f26296d2ccfbc25af4539550496
miwco <ahonemi@arcada.fi>
2013-12-31 12:27:34 +0000

    added blog app

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -62,9 +62,13 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-THIRD_PARTY_APPS = ()
+THIRD_PARTY_APPS = (
+    'south',
+)
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'blog',
+)
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
